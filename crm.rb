9e14e3f2b99c0839cd52f8c6a9d7ffa0333bd94a
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-01-23 20:27:07 -0500

    working on display all contacts method

--- crm.rb
+++ crm.rb
@@ -2,38 +2,38 @@ require_relative './rolodex.rb'
 require_relative './contact.rb'
 
 class CRM #class must have a capital letter!
-  attr_reader :name
-  
-  def initialize(name)
-    @name = name
-    @rolodex = Rolodex.new
+    attr_reader :name
+    
+    def initialize(name)
+      @name = name
+      @rolodex = Rolodex.new
+    end
+
+    def print_main_menu
+      puts "[1] Add a contact"
+      puts "[2] Modify a contact"
+      puts "[3] Display all contacts"
+      puts "[4] Display one contact"
+      puts "[5] Display an attribute"
+      puts "[6] Delete a contact"
+      puts "[7] Exit"	
+      puts "Enter a number"
+    end
+
+    def main_menu
+      puts "Welcome to #{@name}"
+      while true
+        print_main_menu
+      input = gets.chomp.to_i #=> 4
+      choose_option(input)
+      return if input == 7
+    end
   end
 
-def print_main_menu
-  puts "[1] Add a contact"
-  puts "[2] Modify a contact"
-	puts "[3] Display all contacts"
-	puts "[4] Display one contact"
-	puts "[5] Display an attribute"
-	puts "[6] Delete a contact"
-	puts "[7] Exit"	
-  puts "Enter a number"
-end
-
-def main_menu
-  puts "Welcome to #{@name}"
-    while true
-    print_main_menu
-    input = gets.chomp.to_i #=> 4
-    choose_option(input)
-    return if input == 7
-  end
-end
-
-def choose_option(option)
-  # => option = 4
-  #case only does strict ==
-  case option
+  def choose_option(option)
+    # => option = 4
+    #case only does strict ==
+    case option
     when 1 then add_contact
     when 2 then modify_contact
     when 3 then display_all
@@ -44,25 +44,45 @@ def choose_option(option)
       puts "Goodbye!"
       return
     else puts "incorrect option, try again"
+    end
   end
-end
-end
+  
 
-def add_contact
-  puts "Provide contact details"
-  print "First Name:"
-  first_name = gets.chomp
-  print "Last Name:"
-  last_name = gets.chomp
-  print "Email:"
-  email = gets.chomp
-  print "Note:"
-  note = gets.chomp
+  def add_contact
+    puts "Provide contact details"
+    print "First Name:"
+    first_name = gets.chomp
+    print "Last Name:"
+    last_name = gets.chomp
+    print "Email:"
+    email = gets.chomp
+    print "Note:"
+    note = gets.chomp
 
-  new_contact =Contact.new(first_name, last_name, email, note)
-  @rolodex.add_contact(contact)
+    new_contact =Contact.new(first_name, last_name, email, note)
+    @rolodex.add_contact(new_contact)
+  end
+
+  def modify_contact
+    puts "Enter contact ID to modify"
+    modify = gets.chomp.to_i
+
+    found = @rolodex.find_contact(modify)
+    if found.nil?
+      puts "Could not find a contact with that ID"
+    else
+      found.first_name = "John"
+    end
+  end
+
+  def display_all
+    @rolodex.contacts.each do |x|
+      puts "FIRST NAME: #{x.first_name}, LAST NAME: #{x.last_name}, EMAIL: #{x.email}, NOTE: #{x.note}"
+      puts ""
+    end
+  end
 end
 
 bitmaker = CRM.new("Bitmaker labs CRM")
 bitmaker.main_menu
-puts bitmaker.name
+
--- rolodex.rb
+++ rolodex.rb
@@ -1,14 +1,42 @@
 class Rolodex
+	attr_reader :contacts
 	@@ids = 1001
 	def initialize
-		@contact = []
+		@contacts = []
 		# @ids = 1001
 	end
+
 	def add_contact(contact)
 		contact.id = @@ids
 		@contacts << contact
 		@@ids += 1
-		contact
 	end
-	def modify_contact
+	
+	def find_contact(contact_id)
+
+		found_contact = nil
+		@contacts.each do |contact|
+			if contact_id == contact.id
+				found_contact = contact
+			end
+		end
+		return found_contact
+	end
+	
+	def display_all
+	  @contacts
+	end
+	
+	def display_contact
+
+		
+	end
+	
+	def display_attribute
+		
+	end
+
+	def delete_contact
+		
+	end
 end
